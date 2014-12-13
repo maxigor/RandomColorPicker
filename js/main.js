b78a4efb961dd68620faf94061006c575cc2c193
Max Igor <maxigor.ferreira@gmail.com>
2014-12-13 17:03:16 -0200

    add all files

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-function getRandomColor() {
-  var letters = '0123456789ABCDEF'.split('');
-  var color = '#';
-  for (var i = 0; i < 6; i++ ) {
-   	color += letters[Math.round(Math.random() * 16)];
-  }
-  return color;
-}
+// function getRandomColor() {
+//   var letters = '0123456789ABCDEF'.split('');
+//   var color = '#';
+//   for (var i = 0; i < 6; i++ ) {
+//    	color += letters[Math.round(Math.random() * 16)];
+//   }
+//   return document.getElementById('hex').innerHTML=color;
+// }
 
-function draw(x,y){
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	ctx.fillStyle = getRandomColor();
-	ctx.fillRect(x,y,1260,700);
-}
+// function getRandomColor(){
+// 	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+// 	return document.getElementById('');
+// }
+
+// function draw(x,y){
+// 	var canvas = document.getElementById("canvas");
+// 	var ctx = canvas.getContext("2d");
+// 	ctx.fillStyle = getRandomColor();
+// 	ctx.fillRect(x,y,1260,700);
+// }
+
+$(function(){
+	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+	$("body").css({
+		backgroundColor: '#' + randomColor
+	});
+});
